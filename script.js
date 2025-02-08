e6cfbad7ed8c597e4edc83859cc5d08bd41c1b4a
luellem da luz <luellendaluz65@gmail.com>
2025-02-08 18:33:02 -0300

    Resolução do Desafio 1 Trilhas inova

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-// Atribuindo o nome á variável
-let nome = "Luellen";
-console.log(nome)
+//Atribuindo 10 á variável numero1
+let numero1 = 10;
+//Atribuindo 20 á variável numero2
+let numero2 = 20;
+if (numero1 === numero2) {
+    // Mensagem se os números forem iguais
+    console.log ("Os números são iguas") ; 
+} else {
+    // Mensagem se os números forem diferentes
+    console.log ("Os números são diferentes");
+}
